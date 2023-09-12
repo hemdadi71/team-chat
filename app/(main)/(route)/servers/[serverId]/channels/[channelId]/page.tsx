5f545a55307e039552fda3d83ecc9e818a521831
hemdadi71 <hemdadi71@gmail.com>
2023-09-12 22:25:04 +0330

    channel id page fixed

--- app/(main)/(route)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(route)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,50 +1,48 @@
-import { redirectToSignIn } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
-import { ChannelType } from "@prisma/client";
+import { redirectToSignIn } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import { ChannelType } from '@prisma/client'
 
 
-import { ChatHeader } from "@/components/chat/chat-header";
-import { ChatInput } from "@/components/chat/chat-input";
-import { ChatMessages } from "@/components/chat/chat-messages";
-import { MediaRoom } from "@/components/media-room";
-import { currentProfile } from "@/lib/current-pofile";
-import db from "@/lib/db";
+import { ChatHeader } from '@/components/chat/chat-header'
+import { ChatInput } from '@/components/chat/chat-input'
+import { ChatMessages } from '@/components/chat/chat-messages'
+import { MediaRoom } from '@/components/media-room'
+import { currentProfile } from '@/lib/current-pofile'
+import db from '@/lib/db'
 
 
 interface ChannelIdPageProps {
   params: {
-    serverId: string;
-    channelId: string;
+    serverId: string
+    channelId: string
   }
 }
 
-const ChannelIdPage = async ({
-  params
-}: ChannelIdPageProps) => {
-  const profile = await currentProfile();
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  const profile = await currentProfile()
 
   if (!profile) {
-    return redirectToSignIn();
+    return redirectToSignIn()
   }
 
   const channel = await db.channel.findUnique({
     where: {
       id: params.channelId,
     },
-  });
+  })
 
   const member = await db.member.findFirst({
     where: {
       serverId: params.serverId,
       profileId: profile.id,
-    }
-  });
+    },
+  })
 
   if (!channel || !member) {
-    redirect("/");
+    redirect('/')
   }
 
-  return ( 
+  return (
     <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
       <ChatHeader
         name={channel.name}
@@ -79,21 +77,13 @@ const ChannelIdPage = async ({
         </>
       )}
       {channel.type === ChannelType.AUDIO && (
-        <MediaRoom
-          chatId={channel.id}
-          video={false}
-          audio={true}
-        />
+        <MediaRoom chatId={channel.id} video={false} audio={true} />
       )}
       {channel.type === ChannelType.VIDEO && (
-        <MediaRoom
-          chatId={channel.id}
-          video={true}
-          audio={true}
-        />
+        <MediaRoom chatId={channel.id} video={true} audio={true} />
       )}
     </div>
-   );
+  )
 }
- 
-export default ChannelIdPage;
+
+export default ChannelIdPage
